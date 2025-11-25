b0a351905919aaa4d03a3acfa0f8aae2173cfccc
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-11-24 22:53:04 -0600

    feat: nueva pagina de blog

--- main.go
+++ main.go
@@ -27,12 +27,34 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func blogHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/blog" {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("./templates/blog/blog.html")
+	if err != nil {
+		log.Printf("Error al parsear la plantilla: %v", err)
+		http.Error(w, "Error interno del servidor.", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Error al ejecutar la plantilla: %v", err)
+		http.Error(w, "Error al generar la página.", http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
 	log.Println("Starting server on port 8080...")
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/blog", blogHandler)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
