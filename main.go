9954c9d1aec6b229728dbe578132f4681329e775
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-11-24 22:16:14 -0600

    |

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("./templates/index.html")
+	if err != nil {
+		log.Printf("Error al parsear la plantilla: %v", err)
+		http.Error(w, "Error interno del servidor.", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Error al ejecutar la plantilla: %v", err)
+		http.Error(w, "Error al generar la página.", http.StatusInternalServerError)
+		return
+	}
+}
+s
+func main() {
+	log.Println("Starting server on port 8080...")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	http.HandleFunc("/", indexHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Error al iniciar el servidor: ", err)
+	}
+}
