318539d9d03fd98311ee3113e50ce65216ac4aa8
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-12-02 23:20:47 -0600

    feat: proyecto de don roberto

--- main.go
+++ main.go
@@ -1,63 +1,21 @@
 package main
 
 import (
-	"html/template"
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	tmpl, err := template.ParseFiles("./templates/index.html")
-	if err != nil {
-		log.Printf("Error al parsear la plantilla: %v", err)
-		http.Error(w, "Error interno del servidor.", http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("Error al ejecutar la plantilla: %v", err)
-		http.Error(w, "Error al generar la página.", http.StatusInternalServerError)
-		return
-	}
-}
-
-func blogHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/blog" {
-		http.NotFound(w, r)
-		return
-	}
-
-	tmpl, err := template.ParseFiles("./templates/blog/blog.html")
-	if err != nil {
-		log.Printf("Error al parsear la plantilla: %v", err)
-		http.Error(w, "Error interno del servidor.", http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("Error al ejecutar la plantilla: %v", err)
-		http.Error(w, "Error al generar la página.", http.StatusInternalServerError)
-		return
-	}
-}
-
 func main() {
-	log.Println("Starting server on port 8080...")
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	r := gin.Default()
+	r.Static("/assets", "./assets")
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title":   "Hello, World!",
+			"message": "Welcome to my blog!",
+		})
+	})
 
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/blog", blogHandler)
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("Error al iniciar el servidor: ", err)
-	}
+	r.Run(":8080")
 }
