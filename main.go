3550b8b8367da2c49c3aa0075cf30dad6cdfed1e
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-12-06 01:56:44 -0600

    feat: inicio de base de datos

--- main.go
+++ main.go
@@ -1,12 +1,73 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"text/tabwriter"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type Envio struct {
+	ID             int
+	Productos      string
+	Cliente        string
+	TrackingNumber string
+	Estatus        string
+	FechaHora      time.Time
+}
+
 func main() {
+	_ = godotenv.Load()
+
+	connStr := os.Getenv("DATABASE_URL")
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatal("Error conectando a la BD: ", err)
+	}
+
+	rows, err := db.Query("SELECT id, productos, cliente, tracking_number, estatus, fecha_hora FROM envios")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+
+	fmt.Fprintln(w, "ID\tCLIENTE\tTRACKING\tESTATUS\tFECHA\tPRODUCTOS")
+	fmt.Fprintln(w, "--\t-------\t--------\t-------\t-----\t---------")
+
+	for rows.Next() {
+		var e Envio
+		err := rows.Scan(&e.ID, &e.Productos, &e.Cliente, &e.TrackingNumber, &e.Estatus, &e.FechaHora)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fechaFormateada := e.FechaHora.Format("2006-01-02 15:04")
+
+		fmt.Fprintf(w, "%d\t%s\t%s\t%s\t%s\t%s\n",
+			e.ID, e.Cliente, e.TrackingNumber, e.Estatus, fechaFormateada, e.Productos)
+	}
+
+	w.Flush()
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
 	r := gin.Default()
 	r.Static("/assets", "./assets")
 	r.LoadHTMLGlob("templates/*")
